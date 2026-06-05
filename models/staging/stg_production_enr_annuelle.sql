7f5e3db6a5feb64e394b42f8928d043ebe3fa945
sarahsrra@gmail.com <sarahsrra@gmail.com>
2026-06-05 10:49:00 +0000

    maj

--- models/staging/stg_production_enr_annuelle.sql
+++ models/staging/stg_production_enr_annuelle.sql
@@ -4,18 +4,11 @@ with source as (
 
 renamed as (
     select
-        LPAD(cast(code_commune as string), 5, '0')                          as code_insee,
-        cast(annee as string)                                               as annee,
-        cast(energie_solaire_mwh as float64)                                as energie_solaire_mwh,
-        cast(energie_eolien_mwh as float64)                                 as energie_eolien_mwh,
-        cast(energie_hydraulique_mwh as float64)                            as energie_hydraulique_mwh,
-        cast(energie_bio_mwh as float64)                                    as energie_bio_mwh,
-        coalesce(cast(energie_solaire_mwh as float64), 0) +
-        coalesce(cast(energie_eolien_mwh as float64), 0) +
-        coalesce(cast(energie_hydraulique_mwh as float64), 0) +
-        coalesce(cast(energie_bio_mwh as float64), 0)                       as energie_totale_mwh,
-        cast(nb_sites_solaire as int64)                                     as nb_sites_solaire,
-        cast(nb_sites_eolien as int64)                                      as nb_sites_eolien
+        LPAD(cast(code_commune as string), 5, '0')          as code_insee,
+        cast(annee as string)                               as annee,
+        cast(energie_solaire_mwh as float64)                as energie_solaire_mwh,
+        cast(energie_eolien_mwh as float64)                 as energie_eolien_mwh,
+        cast(energie_totale_mwh as float64)                 as energie_totale_mwh
     from source
 )
 
